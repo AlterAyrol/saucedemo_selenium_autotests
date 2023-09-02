08935d7ddbed3309a192b2d8f321a799bc5ea815
AlterAyrol <lazarev.reach@gmail.com>
2023-09-02 19:16:10 +0300

    refactor class to func. Use webdriver from conftest

--- tests/test_about_link.py
+++ tests/test_about_link.py
@@ -1,41 +1,29 @@
 from selenium import webdriver
 import time
 
-from pages.login_page import Login_page
-from pages.products_page import Product_page
-from pages.curt_page import Curt_page
-from pages.your_information_page import Your_information_page
-from pages.last_page import Last_page
-from pages.finish_page import Finish_page
+from pages.loginpage import LoginPage
+from pages.products_page import ProductPage
 
 
+def test_about_link(web_browser):
+    '''Смоук тест по переходу по ссылке about'''
 
-class Test():
+    # user_login_list = ['standard_user', 'locked_out_user', 'problem_user', 'performance_glitch_user']
+    user_login_list = ['standard_user']
+    user_password = 'secret_sauce'
 
-    def test_about_link(self):
-        '''Смоук тест по переходу по ссылке about'''
+    driver = web_browser
 
-        # user_login_list = ['standard_user', 'locked_out_user', 'problem_user', 'performance_glitch_user']
-        user_login_list = ['standard_user']
-        user_password = 'secret_sauce'
+    test_num = 0
 
-        driver = webdriver.Chrome()
+    for user_login in user_login_list:
+        test_num += 1
+        print(f"Start smoke testing №{test_num} with login - {user_login}. Check about link")
 
-        test_num = 0
+        login = LoginPage(driver=driver)
+        login.authorization(login_name=user_login, login_password=user_password)
 
-        for user_login in user_login_list:
-            test_num += 1
-            print(f"Start smoke testing №{test_num} with login - {user_login}. Check about link")
-
-            login = Login_page(driver=driver)
-            login.authorization(login_name=user_login, login_password=user_password)
-
-            time.sleep(1)
-            product_page = Product_page(driver=driver)
-            time.sleep(1)
-            product_page.open_burger_menu_and_go_to_about()
-
-            driver.close()
-
-
-            time.sleep(2)
+        time.sleep(1)
+        product_page = ProductPage(driver=driver)
+        time.sleep(1)
+        product_page.open_burger_menu_and_go_to_about()
